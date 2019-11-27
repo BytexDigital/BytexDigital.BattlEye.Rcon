235cc84da5adaa958e034a2d310d6a3d9a2bf32a
Ryan Torzynski <ryantorzynski@gmail.com>
2019-11-27 02:47:21 +0100

    Changed encoding to UTF8

--- BytexDigital.BattlEye.Rcon/StringEncoder.cs
+++ BytexDigital.BattlEye.Rcon/StringEncoder.cs
@@ -4,7 +4,7 @@ namespace BytexDigital.BattlEye.Rcon
 {
     public class StringEncoder
     {
-        private readonly Encoding _encoding = Encoding.GetEncoding(1252);
+        private readonly Encoding _encoding = Encoding.UTF8;
 
         public byte[] GetBytes(string message) => _encoding.GetBytes(message);
         public string GetString(byte[] bytes) => _encoding.GetString(bytes);

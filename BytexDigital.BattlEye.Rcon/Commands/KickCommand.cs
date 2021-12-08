6d56dd3529e62abc8bc1ea0b0381c5768eff53e9
Ryan <ryantorzynski@gmail.com>
2021-12-08 16:43:19 +0100

    Added reason to KickCommand

--- BytexDigital.BattlEye.Rcon/Commands/KickCommand.cs
+++ BytexDigital.BattlEye.Rcon/Commands/KickCommand.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BytexDigital.BattlEye.Rcon.Commands
+﻿namespace BytexDigital.BattlEye.Rcon.Commands
 {
     /// <summary>
     /// Requests the server to remove a player from the server.
@@ -14,6 +10,15 @@ namespace BytexDigital.BattlEye.Rcon.Commands
         /// </summary>
         /// <param name="playerId"></param>
         public KickCommand(int playerId) : base($"kick {playerId}")
+        {
+        }
+
+        /// <summary>
+        /// Requests the server to remove the given player ID from the server with the specified reason.
+        /// </summary>
+        /// <param name="playerId"></param>
+        /// <param name="reason"></param>
+        public KickCommand(int playerId, string reason) : base($"kick {playerId} {reason}")
         {
         }
     }
